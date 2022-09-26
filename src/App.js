b2f3d9ae0f8bfdf0b30d013179c0929ea0ea2de0
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-09-26 17:36:22 +0100

    set out div structure of app

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ function App() {
   // const url = `https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=${process.env.REACT_APP_API_KEY}`
 
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Bristol</p>
+          </div>
+          <div className='temp'>
+            <h1>18°C</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'>
+            <p>21°C</p>
+          </div>
+          <div className='humidity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
