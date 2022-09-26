4c18b55651e0d63d9a579cb0b6836174808bdf71
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-09-26 17:27:32 +0100

    add API url and key

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function App() {
+
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=${process.env.REACT_APP_API_KEY}`
+
   return (
     <div className="App">
 
