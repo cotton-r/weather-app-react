aa44673f24cb4ce4597756c437339ed16c4204fa
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-09-26 17:13:06 +0100

    install axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+
     </div>
   );
 }
