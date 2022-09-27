2e89b199bf633b5458d90410993bfbb3a7c579bc
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-09-27 16:37:38 +0100

    add further classes to sub text for styling

--- src/App.js
+++ src/App.js
@@ -21,13 +21,16 @@ function App() {
         </div>
         <div className='bottom'>
           <div className='feels'>
-            <p>21°C</p>
+            <p className='bold'>21°C</p>
+            <p className='sub-desc'>Feels Like</p>
           </div>
           <div className='humidity'>
-            <p>20%</p>
+            <p className='bold'>20%</p>
+            <p className='sub-desc'>Humidity</p>
           </div>
           <div className='wind'>
-            <p>12 MPH</p>
+            <p className='bold'>12 MPH</p>
+            <p className='sub-desc'>Wind Speed</p>
           </div>
         </div>
       </div>
