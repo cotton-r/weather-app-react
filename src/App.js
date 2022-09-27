f7ea886add4cb8bdbcd0ae012631d1b84146d7e9
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-09-27 16:41:29 +0100

    create search function using axios to call API

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import axios from 'axios';
 
 function App() {
 
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=bristol&appid=${process.env.REACT_APP_API_KEY}`
+  const [data, setData] = useState({});
+
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_API_KEY}`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setImmediate(response.data)
+        console.log(response.data)
+      })
+    }
+  };
 
   return (
     <div className="app">
